514ec92519619da94755ca45cd4cc44de06abdaf
kindlebit-php <kindlebit.php@kindlebit.com>
2023-09-20 15:29:51 +0530

    changes

--- controllers/BookingController.js
+++ controllers/BookingController.js
@@ -30,22 +30,22 @@ export const customer_booking = async(req,res)=>{
                     // return false;
                     var sql = "INSERT INTO bookings (user_id,delievery_day,date,time,total_loads,order_type,driver_id) VALUES ('"+userData[0].id+"','"+delievery_day+"', '"+date+"', '"+current_time+"','"+total_loads+"','"+order_type+"',52)";
                     dbConnection.query(sql, function (err, result) {
-                        // if (err) throw err;
+                        // // if (err) throw err;
 
-                        for (var i = 0; total_loads > i; i++) {
-                        var sql = "INSERT INTO booking_qr (booking_id,qr_code) VALUES ('"+result.insertId+"','"+randomNumber(result.insertId)+"')";
-                        dbConnection.query(sql, function (err, result) {
-                        // if (err) throw err;
-                        // res.json({'status':true,"message":"Booking added successfully!"});
-                        });     
-                        }
+                        // for (var i = 0; total_loads > i; i++) {
+                        // var sql = "INSERT INTO booking_qr (booking_id,qr_code) VALUES ('"+result.insertId+"','"+randomNumber(result.insertId)+"')";
+                        // dbConnection.query(sql, function (err, result) {
+                        // // if (err) throw err;
+                        // // res.json({'status':true,"message":"Booking added successfully!"});
+                        // });     
+                        // }
 
 
-                        var order_id = '1001'+result.insertId;
-                        var sql = "update bookings set order_id = '"+order_id+"'where id = '"+result.insertId+"'";
-                        dbConnection.query(sql, function (err, results) {
-                        // if (err) throw err;
-                        }); 
+                        // var order_id = '1001'+result.insertId;
+                        // var sql = "update bookings set order_id = '"+order_id+"'where id = '"+result.insertId+"'";
+                        // dbConnection.query(sql, function (err, results) {
+                        // // if (err) throw err;
+                        // }); 
                         res.json({'status':true,"message":"Booking added successfully!"});
                     }); 
                 }
