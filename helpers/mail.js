00ba8c0d7150cb117c8e3b6900388446f8243e3e
kindlebit-php <kindlebit.php@kindlebit.com>
2023-09-26 13:13:19 +0530

    changes

--- helpers/mail.js
+++ helpers/mail.js
@@ -1,12 +1,20 @@
 import nodemailer from "nodemailer";
 
 //nodemail credential
+// var transport = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 25,
+//   auth: {
+//     user: "weclea2023@gmail.com",
+//     pass: "gugp jorx avii pnkv"
+//   }
+// });
 var transport = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 25,
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
   auth: {
-    user: "weclea2023@gmail.com",
-    pass: "gugp jorx avii pnkv"
+    user: "1318c776b0e0a6",
+    pass: "e5a0badddf664c"
   }
 });
 
