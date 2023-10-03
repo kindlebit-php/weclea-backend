3dba21bd4bc8d07a899714555289075603b5593e
kindlebit-php <kindlebit.php@gmail.com>
2023-10-03 17:58:29 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
-import mysql from 'mysql'; // Import from 'mysql2/promise'
-
-import dotenv from "dotenv";
+import mysql from 'mysql';
+import dotenv from 'dotenv';
 dotenv.config();
+
 const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_DATABASE = process.env.DB_DATABASE;
 
-const connection = mysql.createConnection({
+const dbConnect = mysql.createConnection({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
@@ -16,16 +16,18 @@ const connection = mysql.createConnection({
   dateStrings: true,
 });
 
-try {
-  connection.connect();
-  console.log('Database Connection Established Successfully');
-} catch (error) {
-  console.error('Database Connection Failed!', error);
-}
+// Connect to the database
+dbConnect.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    throw err; // Terminate the application or handle the error as needed
+  }
+  console.log('Connected to the database');
+});
 
 // Handle unexpected errors
-connection.on('error', (err) => {
+dbConnect.on('error', (err) => {
   console.error('Database Error:', err);
 });
 
-export default connection;
+export default dbConnect;
