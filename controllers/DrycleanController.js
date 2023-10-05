f347ea5ddcf5f63bf78ce157813bbd43e440e5f0
kindlebit-php <kindlebit.php@kindlebit.com>
2023-10-05 16:00:31 +0530

    dry clean booking

--- controllers/DrycleanController.js
+++ controllers/DrycleanController.js
@@ -1,4 +1,6 @@
 import dbConnection from'../config/db.js';
+import dateFormat from 'date-and-time';
+
 export const get_category = async (req, res) => {
     try {
       var resData = [];
@@ -91,7 +93,50 @@ export const get_category = async (req, res) => {
       res.json({ status: false, message: error.message });
     }
   }
+
+  export const dry_clean_booking = async (req, res) => {
+    try {
+        const userData = res.user;
+        const { delievery_day,date,amount} = req.body;
+        if(  delievery_day && date && amount){
+        let dateObject = new Date();
+        let hours = dateObject.getHours();
+        let minutes = dateObject.getMinutes();
+        const current_time = hours + ":" + minutes;
+        const oneTimeDate = dateFormat.format(new Date(date),'YYYY-MM-DD');
+
+        var sql = "INSERT INTO bookings (user_id,delievery_day,date,time,order_type,driver_id,category_id,total_amount) VALUES ('"+userData[0].id+"','"+delievery_day+"', '"+oneTimeDate+"', '"+current_time+"','3',52,'"+userData[0].category_id+"','"+amount+"')";
+        dbConnection.query(sql, function (err, result) {
+
+        var order_id = '1001'+result.insertId;
+        var sql = "update bookings set order_id = '"+order_id+"'where id = '"+result.insertId+"'";
+        dbConnection.query(sql, function (err, resultss) {
+
+        });
+
+        const updateService = "update cart set booking_id = '"+result.insertId+"' where user_id = '"+userData[0].id+"' and status = '0'";
+        dbConnection.query(updateService, function (error, data) {
+             if(error) throw error;
+              res.json({'status':true,"message":"booking created successfully",'booking_id':result.insertId});  
+              
+            });
+
+        });     
+        }else{
+            res.json({'status':false,"message":"All fields are required"});
+      }             
+    }catch (error) {
+      res.json({ status: false, message: error.message });
+    }
+  }
+
   
 
-  export default {get_category ,Add_To_Cart,delete_cart_item,get_cart_items}
+  export default {
+    get_category,
+    Add_To_Cart,
+    delete_cart_item,
+    get_cart_items,
+    dry_clean_booking
+  }
   
