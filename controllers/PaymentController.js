eec4c4314477a9154f74f35201b3105c53aa74e3
solankisgr <sagar.solanki@kindlebit.net>
2023-10-13 13:20:47 +0530

    changes3

--- controllers/PaymentController.js
+++ controllers/PaymentController.js
@@ -948,8 +948,15 @@ export const customer_extra_payment = async (req, res) => {
           const insertData = "INSERT INTO payment (user_id, amount, payment_id, date) VALUES ('" + userId + "','" + amount + "','" + paymentIntent.id + "','" + currentDate + "')";
           dbConnection.query(insertData, function (err, result) {
             if (err) throw err;
-            if (result) {
-              return res.json({ status: true, message: 'Payment successful',paymentId:result.insertId});
+            else {
+              const updatedStatus = 'UPDATE users SET card_status = 1 WHERE id = ?';
+              dbConnection.query(updatedStatus, [userId], (err, results) => {
+                if (err) {
+                  return res.json({ status: false, message: 'Failed to update payment status' });
+                }
+                return res.json({ status: true, message: 'Payment successful',paymentId:result.insertId});
+              });
+             
             }
           });
         } else {
