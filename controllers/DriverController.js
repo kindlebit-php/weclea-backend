5b2882f50601fc863ef57eab1b107459afa92427
solankisgr <sagar.solanki@kindlebit.net>
2023-10-17 12:34:39 +0530

    status

--- controllers/DriverController.js
+++ controllers/DriverController.js
@@ -91,7 +91,7 @@ export const print_All_Drop_QrCode = async (req, res) => {
       if (error) {
         return res.json({ status: false, message: error.message });
       }else if(data.length == 0 &&  data[0].driver_drop_status == '1'){
-        return res.json({ status: false, message: "All loads already scanned!!" })
+        return res.json({status:1})
       }else{
       res.json({status: true,message: "Data retrieved successfully!", data: data });
       }
