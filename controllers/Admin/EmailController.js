3c7b2fc54f6e00a2b3a62df6309c9a825964b544
HarryTEK10 <harry@teknomines.com>
2023-10-31 13:41:42 +0530

    Admin email api integration- 31-oct-23

--- controllers/Admin/EmailController.js
+++ controllers/Admin/EmailController.js
@@ -11,7 +11,7 @@ const BUCKET_NAME = 'weclea-bucket';
      });
 
 
-/********  Rating feedback suggesstion  ***********/
+/********  Email template suggesstion  ***********/
 export const get_emailTemplate = async(req,res)=>{
       try { 
         	const loads = "select * from wc_email_template order by id desc";
@@ -25,7 +25,7 @@ export const get_emailTemplate = async(req,res)=>{
 }
 export const get_emailTemplate_detail = async(req,res)=>{
       try { 
-        	const loads = "select * from wc_email_template  where wc_rating_feeback.isDelete=0 order by wc_rating_feeback.create_date desc";
+        	const loads = "select * from wc_email_template  where wc_email_template.status=0 order by wc_rating_feeback.create_date desc";
 			dbConnection.query(loads, function (error, data) {
 			if (error) throw error;
 				res.json({'status':true,"message":"Success",'data':data});
@@ -38,17 +38,17 @@ export const update_emailTemplate = async(req,res)=>{
 	const reqData = req.body;
 	//`rating_id`, `feedback`
     try { 
-    	const qrySelect = "select id from wc_rating_feeback where `rating_id`=? and `feedback`=? and status=1 and id!=?";
+    	const qrySelect = "select id from wc_email_template where `subject`=? and status=0 and id!=?";
 		dbConnection.query(qrySelect,[reqData.rating_id, reqData.feedback, reqData.id], function (error, data) {
 		if (error) throw error;
 			if (data.length<=0) { ///`category_id`, `type`, `loads`, `min_load_per_day`, `price`,
-			    var updateContnetQry = "update wc_rating_feeback set `rating_id`=?, `feedback`=? where id = ? ";
-			    dbConnection.query(updateContnetQry,[reqData.rating_id, reqData.feedback,reqData.id], function (error, data) {
+			    var updateContnetQry = "update wc_email_template set `subject`=?, `body`=? where id = ? ";
+			    dbConnection.query(updateContnetQry,[reqData.subject, reqData.body,reqData.id], function (error, data) {
 				if (error) throw error;
-					res.json({'status':true,"message":"Feedback Suggestion has been updated successfully",'data':data});
+					res.json({'status':true,"message":"Email template has been updated successfully",'data':data});
 				});
 			}else{
-				res.json({'status':false,"message":"Same Suggestion already exist"});
+				res.json({'status':false,"message":"Same Email subject already exist"});
 			}
 		})
     }catch (error) {
@@ -58,17 +58,17 @@ export const update_emailTemplate = async(req,res)=>{
 export const create_emailTemplate = async(req,res)=>{
 	const reqData = req.body;
     try { 
-    	const qrySelect = "select id from wc_rating_feeback where `rating_id`=? and `feedback`=? and status=1";
-		dbConnection.query(qrySelect,[reqData.category_id, reqData.type, reqData.loads, reqData.min_load_per_day, reqData.price], function (error, data) {
+    	const qrySelect = "select id from wc_email_template where `subject`=? and `body`=? and status=0";
+		dbConnection.query(qrySelect,[reqData.subject, reqData.body], function (error, data) {
 		if (error) throw error;
 			if (data.length<=0) {
-			    var addContnetQry = "insert wc_rating_feeback set `rating_id`=?, `feedback`=? ";
-			    dbConnection.query(addContnetQry,[reqData.rating_id, reqData.feedback], function (error, data) {
+			    var addContnetQry = "insert wc_email_template set `subject`=?, `body`=? ";
+			    dbConnection.query(addContnetQry,[reqData.subject, reqData.body], function (error, data) {
 				if (error) throw error;
-					res.json({'status':true,"message":"Feedback Suggestion has been saved successfully",'data':data});
+					res.json({'status':true,"message":"Email template has been saved successfully",'data':data});
 				});
 			}else{
-				res.json({'status':false,"message":"Same Suggestion already exist"});
+				res.json({'status':false,"message":"Same email template already exist"});
 			}
 		});
     }catch (error) {
@@ -79,7 +79,7 @@ export const create_emailTemplate = async(req,res)=>{
 export const delete_emailTemplate = async(req,res)=>{
 	const reqData = req.body;
     try { 
-    	const qrySelect = "select id from wc_rating_feeback where id=?";
+    	const qrySelect = "select id from wc_email_template where id=?";
 		dbConnection.query(qrySelect,[reqData.id], function (error, data) {
 		if (error) throw error;
 			if (data.length>0) {
@@ -87,7 +87,7 @@ export const delete_emailTemplate = async(req,res)=>{
 				if (reqData.isDelete==1) {
 					msg= "Feedback Suggestion has been deleted successfully"
 				}
-			    var updateContnetQry = "update wc_rating_feeback set isDelete=? where id=? ";
+			    var updateContnetQry = "update wc_email_template set isDelete=? where id=? ";
 			    dbConnection.query(updateContnetQry,[reqData.isDelete,reqData.id], function (error, data) {
 				if (error) throw error;
 					res.json({'status':true,"message":msg,'data':data});
@@ -104,15 +104,15 @@ export const delete_emailTemplate = async(req,res)=>{
 export const update_emailTemplate_status = async(req,res)=>{
 	const reqData = req.body;
     try { 
-    	const qrySelect = "select id from wc_rating_feeback where id=?";
+    	const qrySelect = "select id from wc_email_template where id=?";
 		dbConnection.query(qrySelect,[reqData.id], function (error, data) {
 		if (error) throw error;
 			if (data.length>0) {
-				var msg= "Feedback Suggestion has been deactivated successfully"
+				var msg= "Email template has been deactivated successfully"
 				if (reqData.status==1) {
-					msg= "Feedback Suggestion has been activated successfully"
+					msg= "Email template has been activated successfully"
 				}
-			    var updateContnetQry = "update wc_rating_feeback set status=? where id=? ";
+			    var updateContnetQry = "update wc_email_template set status=? where id=? ";
 			    dbConnection.query(updateContnetQry,[reqData.status,reqData.id], function (error, data) {
 				if (error) throw error;
 					res.json({'status':true,"message":msg,'data':data});
@@ -126,7 +126,7 @@ export const update_emailTemplate_status = async(req,res)=>{
     }
 }
 
-/****** end feedback section*******/
+/****** end Eamil section*******/
 export default {
 	get_emailTemplate,
 	get_emailTemplate_detail,
