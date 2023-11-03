7fd5354462a913097a978ac6952ac97169245e20
kindlebit-php <kindlebit.php@kindlebit.com>
2023-11-03 16:33:21 +0530

    changes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -773,7 +773,6 @@ export const order_list = async (req, res) => {
 		}else if (item.order_images === 8) {
 			const imagesResults = await new Promise((resolve, reject) => {
 				dbConnection.query(imagesQuery, (error, Data) => {
-					console('finaldata',Data)
 				  if (error) {
 					reject(error);
 				  } else {
