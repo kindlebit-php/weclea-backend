d116b55b2b39874e93e490791431adf9e7f85941
HarryTEK10 <harry@teknomines.com>
2023-11-07 12:46:16 +0530

    Admin group api integration- 07-Nov-23

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -166,6 +166,7 @@ router.post("/update_emailTemplate_status",CheckAuth,emailController.update_emai
 router.get("/get_group_list",CheckAuth,groupController.get_group_list)
 router.post("/create_group",uploadS3.array('profile_pic',25),groupController.create_group);
 router.post("/update_group",uploadS3.array('profile_pic',25),groupController.update_group);
+router.post("/delete_group",CheckAuth,groupController.delete_group)
 
 	
 /*****  ---------- Admin API End ---------- *****/
