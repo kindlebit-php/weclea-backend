db19418d37e4db79b33babfc5ad9c0e6cfc4100a
kindlebit-php <kindlebit.php@kindlebit.com>
2023-11-08 17:34:38 +0530

    chnages

--- controllers/BookingController.js
+++ controllers/BookingController.js
@@ -584,6 +584,34 @@ export const booking_tracking_details = async(req,res)=>{
                         resPackImg[key] = process.env.BASE_URL+'/uploads/'+img;
                     })
                 }
+                        if(order_status == 1){
+            var wash_status = 1;
+        }else if(order_status == 2){
+            var dry_status = 1;
+            var wash_status = 1;
+        }else if(order_status == 3){
+            var fold_status = 1;
+            var dry_status = 1;
+            var wash_status = 1;
+        }else if(order_status == 4){
+            var pack_status = 1;
+            var fold_status = 1;
+            var dry_status = 1;
+            var wash_status = 1;
+        }else if(order_status == 6){
+            var pack_status = 1;
+            var dry_status = 1;
+            var fold_status = 1;
+            var wash_status = 1;
+            var statusN = 1
+        }else{
+            var pack_status = 0;
+            var dry_status = 0;
+            var fold_status = 0;
+            var wash_status = 0;
+            var statusN = 0
+        }
+
 
                     const laundry_detail = [
                       {
