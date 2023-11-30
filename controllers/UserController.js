cb3d7b2db3c28ee9597c3133ed8882c268c8160d
solankisgr <sagar.solanki@kindlebit.net>
2023-11-30 15:20:43 +0530

    pdf-api

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1941,7 +1941,7 @@ export const customer_list = async (req, res) => {
 				}
 	
 				if (qrCountresults[0].qrCount > 0) {
-					const pdfTable = (order_type === 3) ? 'dry_clean_booking_qr' : 'booking_qr';
+					const pdfTable = (order_type == 3) ? 'dry_clean_booking_qr' : 'booking_qr';
 					const pdfSql = `SELECT pdf FROM ${pdfTable} WHERE booking_id = ?`;
 	
 					dbConnection.query(pdfSql, [booking_Id], async (pdfError, pdfResult) => {
@@ -1949,7 +1949,6 @@ export const customer_list = async (req, res) => {
 							console.error(pdfError);
 							return res.json({ status: false, message: "An error occurred while retrieving PDF link." });
 						}
-	
 						res.json({ message: "PDF link retrieved", pdf: pdfResult[0].pdf });
 					});
 				} else {
