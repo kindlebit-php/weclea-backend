347f687d2b0992cb76ba19b6e59c089f3d6350c7
HarryTEK10 <harry@teknomines.com>
2023-12-01 13:39:39 +0530

    Admin multip cityapi integration- 01-Dec-23

--- controllers/Admin/GroupController.js
+++ controllers/Admin/GroupController.js
@@ -27,7 +27,7 @@ export const get_group_list = async(req,res)=>{
 export const get_county_list = async(req,res)=>{
 	var reqData= req.params
     try { 
-    	const loads = "select wc_county.*, wc_states.name state_name,wc_cities.name city_name from wc_county LEFT JOIN wc_cities on wc_cities.id=wc_county.city_id LEFT JOIN wc_states on wc_states.id=wc_county.state_id where wc_county.status=1 and wc_county.isDeleted=0 and wc_county.state_id=? order by wc_county.name asc;";
+    	const loads = "select wc_county.*, wc_states.name state_name,wc_cities.name city_name from wc_county LEFT JOIN wc_cities on wc_cities.id=wc_county.city_id LEFT JOIN wc_states on wc_states.id=wc_county.state_id where wc_county.status=1 and wc_county.isDeleted=0 and wc_county.state_id=?  Group bywc_county.name order by wc_county.name asc";
 		dbConnection.query(loads,[reqData.state_id], function (error, data) {
 		if (error) throw error;
 			res.json({'status':true,"message":"Success",'data':data});
@@ -38,7 +38,7 @@ export const get_county_list = async(req,res)=>{
 }
 export const get_all_county_list = async(req,res)=>{
     try { 
-    	const loads = "select wc_county.*,wc_states.id state_id, wc_states.name state_name,wc_cities.name city_name from wc_county LEFT JOIN wc_cities on wc_cities.id=wc_county.city_id LEFT JOIN wc_states on wc_states.id=wc_cities.state_id where  wc_county.isDeleted=0 order by wc_county.name asc";
+    	const loads = "select wc_county.*,wc_states.id state_id, wc_states.name state_name,wc_cities.name city_name from wc_county LEFT JOIN wc_cities on wc_cities.id=wc_county.city_id LEFT JOIN wc_states on wc_states.id=wc_cities.state_id where  wc_county.isDeleted=0 Group by wc_county.name order by wc_county.name asc";
 		dbConnection.query(loads, function (error, data) {
 		if (error) throw error;
 			res.json({'status':true,"message":"Success",'data':data});
@@ -108,7 +108,7 @@ export const create_county = async(req,res)=>{
 					});
 				    k++;
 				}else{
-					
+
 					//res.json({'status':false,"message":"Same county already exist"});
 				}
 				
